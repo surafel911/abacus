8678323a965cd63f09bc771fa9f8de6f46b4eaf8
Surafel Assefa <surafel911@gmail.com>
2018-05-13 02:09:20 -0400

    Added new attributes.

--- include/macros_clang.h
+++ include/macros_clang.h
@@ -2,7 +2,11 @@
 #define ABACUS_MACROS_CLANG_H
 
 #ifndef __aligned
-#define __aligned(x)	__attribute__((align_value(x)))
+	#define __aligned(x)	__attribute__((align_value(x)))
 #endif // __aligned
+
+#ifndef __noreturn
+	#define __noreturn		__attribute__((noreturn))
+#endif // __noreturn
 
 #endif // ABACUS_MACROS_CLANG_H
--- include/macros_gnu.h
+++ include/macros_gnu.h
@@ -2,7 +2,11 @@
 #define ABACUS_MACROS_GNU_H
 
 #ifndef __aligned
-#define __aligned(x)	__attribute__((aligned (x)))
+	#define __aligned(x)	__attribute__((aligned (x)))
 #endif // __aligned
+
+#ifndef __noreturn
+	#define __noreturn		__attribute__((noreturn))
+#endif // __noreturn
 
 #endif // ABACUS_MACROS_GNU_H 
