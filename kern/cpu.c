d8f1c1663da0eea9f78c5a8096cff2bf5c94354a
Surafel Assefa <surafel911@gmail.com>
2018-07-15 22:10:48 -0400

    Merging latest branch with master.

--- kern/cpu.c
+++ kern/cpu.c
@@ -1,5 +1,22 @@
 #include <cpu.h>
+#include <muart.h>
 
 #include <stdint.h>
 
+void
+cpu_fiq_handler(void)
+{
+	muart_send('F');
+}
 
+void
+cpu_irq_handler(void)
+{
+	muart_send('I');
+}
+
+void
+cpu_synchronous_handler(const uint32_t esr)
+{
+	muart_send('E');
+}
