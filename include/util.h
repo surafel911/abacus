97956aeefef8ed6dc1a6fbfd90249661e50381d8
Surafel Assefa <surafel911@gmail.com>
2018-12-25 10:04:35 -0500

    Implements putc.

--- include/util.h
+++ include/util.h
@@ -2,6 +2,9 @@
 #define ABACUS_UTIL_H
 
 void
+putc(const char c);
+
+void
 puts(const char* s);
 
 #endif // ABACUS_UTIL_H
--- kern/util.c
+++ kern/util.c
@@ -3,6 +3,12 @@
 #include <muart.h>
 
 void
+putc(const char c)
+{
+	muart_send(c);
+}
+
+void
 puts(const char* s)
 {
 	while (*s != '\0') {
