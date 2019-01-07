82a44aafbdc8ce3d7893523a37ece1241b2d9265
Surafel Assefa <surafel911@gmail.com>
2019-01-07 05:55:54 -0500

    Implements kernel setup function.

--- kern/main.c
+++ kern/main.c
@@ -9,18 +9,19 @@ static char cc;
 void
 kern_setup(void)
 {
+	gpio_output_set_pin(GPIO_PIN_24, true);
+
+	gpio_fsel_set_pin(GPIO_14, GPIO_FSEL_ALT5);
+	gpio_fsel_set_pin(GPIO_15, GPIO_FSEL_ALT5);
+	gpio_pud_set_pins(GPIO_PUD_DOWN, GPIO_PIN_14 | GPIO_PIN_15);
+
+	muart_setup();
 }
 
 void
 kern_main(void)
 {
-	gpio_output_set_pin(GPIO_PIN_24, true);
-
-	gpio_fsel_set_pin(GPIO_14, GPIO_FSEL_ALT5);
-	gpio_fsel_set_pin(GPIO_15, GPIO_FSEL_ALT5);
-	gpio_pud_set_state(GPIO_PUD_DOWN, GPIO_PIN_14 | GPIO_PIN_15);
-
-	muart_setup();
+	kern_setup();
 
 	putc(cc + 'A');
 
